482030550979ff2dd41994e7b7a24e86faeeeec5
lakshay singhal <91775547+plakshay@users.noreply.github.com>
2025-01-25 18:16:12 +0530

    similarity search with sentence transformers and formula

--- server/main.py
+++ server/main.py
@@ -1,18 +1,26 @@
 from fastapi import FastAPI # type: ignore
 from pydantic_models.chat_body import ChatBody
+from services.sort_source_service import SortSourceService
 from services.search_service import SearchService # type: ignore
 
 app = FastAPI()
 
 search_services = SearchService()
+sort_source_services = SortSourceService()
+
 #chat
 # /chat?query = Who%20is%Lakshay? - this is the way the string will be given to the query, therefore pydantic models are setup
 @app.post("/chat")
 def search_endpoint(body : ChatBody):
     # steps to achieve the results 
     # 1. search the web and scrape the appropriate resources based on the body.query(user input)
-    search_results= search_services.web_search(body.query)
-    # 2. sort the sources(according to the least to the most important)
+    search_results = search_services.web_search(body.query)
+    # 2. sort the sources(according to the least to the most important) - similarity search (cosine similarity)
+    sorted_results = sort_source_services.sort_sources(body.query, search_results)
+    print(sorted_results)
     # 3. generate the response using LLM (gemini)
-    print(search_results)
+    
     return body.query
+
+
+# all these comments are made by me, in order to learn, this project is inspired by RIVAAN RANAWAT not by ChatGpt
